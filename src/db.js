996d5948da39ea6b279ff62f1456170a0ed3ef6c
GreenProg-1239 <142376733+GreenProg-1239@users.noreply.github.com>
2026-05-09 17:36:26 +0900

    :feat db참조 수정

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 5432),
-  database: process.env.DB_NAME || 'reservation',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'ptlsaudgh9@l',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = { pool };
